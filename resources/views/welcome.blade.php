7b1f3aed024816fce4d93de49b39a95add0c9b2f
Jon Santos <jonsantox@gmail.com>
2020-01-15 10:34:42 +0100

    Eliminar homestead y pruebas de npm

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -16,7 +16,7 @@
         <!-- Styles -->
         <style>
             html, body {
-                background-color: #fff;
+                /*background-color: #fff;*/
                 color: #636b6f;
                 font-family: 'Nunito', sans-serif;
                 font-weight: 200;
