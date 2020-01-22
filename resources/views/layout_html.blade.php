a87be7dff9c76369d182f481ce43b31e588bb1fe
Julen Prieto <prietojulen@gmail.com>
2020-01-22 14:07:39 +0100

    ico

--- resources/views/layout_html.blade.php
+++ resources/views/layout_html.blade.php
@@ -8,6 +8,7 @@
     <link href="https://fonts.googleapis.com/css?family=Heebo&display=swap" rel="stylesheet">
     <link href="{{asset('css/app.css')}}" rel="stylesheet">
     <link href="{{secure_asset('css/app.css')}}" rel="stylesheet">
+    <link rel="shortcut icon" href="{{URL::asset('images/road-tech-assistance.ico')}}}" type="image/x-icon">
     <script src="https://kit.fontawesome.com/122a23edb6.js" crossorigin="anonymous"></script>
     <style>
         body {
